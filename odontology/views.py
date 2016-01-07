42a2d898b9abba44180edd0aadf4f805aacb66eb
Nonato Sales <nrdesales@hotmail.com>
2016-01-07 15:50:51 -0300

    crud ok, register Dentist

--- odontology/views.py
+++ odontology/views.py
@@ -16,29 +16,40 @@ def dentist_index(request):
 	dentists = Dentist.objects.all()
 	return render(request, 'odontology/dentist/dentist_index.html', { 'dentists': dentists }, context_instance=RequestContext(request))
 
+# New e Edit - Dentist
 def dentist_register(request, user_id=None):
-	dentist = Dentist.objects.get(pk=user_id)
-	address = None
-
-	try:
-		address = Address.objects.get(dentist=dentist)
-	except Address.DoesNotExist:
-		address = Address(dentist=dentist)
-
+	
+	# Edit Dentist
 	if user_id:
 		dentist = Dentist.objects.get(pk=user_id)
-
-	form_dentist = DentistForm(instance=dentist)
-	form_address = AddressForm(instance=address)
-	
+		try:
+			address = Address.objects.get(dentist=dentist)
+		except Address.DoesNotExist:
+			address = Address(dentist=dentist)
+		
+		form_dentist = DentistForm(instance=dentist)
+		form_address = AddressForm(instance=address)
+	else: # New Dentist
+		form_dentist = DentistForm
+		form_address = AddressForm
+		
+	# Save	
 	if request.method == 'POST':
-		form_dentist = DentistForm(request.POST, instance=dentist)
-		form_address = AddressForm(request.POST,instance=address)
-		if form_dentist.is_valid():
-			form_dentist.save()
-		if form_address.is_valid():
-			form_address.save()
-			return redirect('dentist_index')
+		if user_id: # Edit
+			form_dentist = DentistForm(request.POST, instance=dentist)
+			form_address = AddressForm(request.POST,instance=address)
+			if form_dentist.is_valid():
+				form_dentist.save()
+			if form_address.is_valid():
+				form_address.save()
+		else: # New
+			form_dentist = DentistForm(request.POST)
+			if form_dentist.is_valid():
+				dentist = form_dentist.save()
+				form_address = AddressForm(request.POST,instance=Address(dentist=dentist))
+			if form_address.is_valid():
+				form_address.save()
+		return redirect('dentist_index')
 
 	return render(request, 'odontology/dentist/dentist_register.html', {'form_dentist': form_dentist, 'form_address': form_address}, context_instance=RequestContext(request))
 	
